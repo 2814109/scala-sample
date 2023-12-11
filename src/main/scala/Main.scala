ce1c019487f6fc95b454f4d730b7ce8b3a04a8e1
2814109 <2814109k@gmail.com>
2023-12-11 21:13:47 +0900

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,10 @@
 @main def hello: Unit =
   println("Hello world!")
   println(msg)
+  println(sample)
 
 def msg = "I was compiled by Scala 3. :)"
+
+
+def sample : String =
+  return "hello GoodMoring"
