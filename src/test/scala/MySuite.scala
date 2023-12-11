59a286c945be8c78cdd15ca4911cbb34d9d6e04b
2814109 <2814109k@gmail.com>
2023-12-11 21:02:45 +0900

    u

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -7,3 +7,11 @@ class MySuite extends munit.FunSuite {
     assertEquals(obtained, expected)
   }
 }
+
+class MySuite2 extends munit.FunSuite {
+  test("example test that succeeds") {
+    val obtained = 42
+    val expected = 42
+    assertEquals(obtained, expected)
+  }
+}
